cf410255e3ee15877c815509ed2f4e1e0ef0c271
A. Saferstein <adriansafer@gmail.com>
2021-01-21 17:45:04 -0300

    Fixed the script and added some inital style ideas for color and bg

--- script.js
+++ script.js
@@ -79,19 +79,26 @@ window.setInterval(greetings, 400);
 
 /*Accordion*/
 
-let accordions = document.querySelectorAll('.accordion');
+let accordions = document.querySelectorAll(".accordion");
+console.log(accordions);
 
-let showAnswer = () => {
-
+function createAccordion() {
+    // get attribute value
     let ariaAttribute = this.getAttribute('aria-expanded');
 
+    // change all accordions aria-expanded attribute value into false.
     for (let i = 0; i < accordions.length; i++) {
         accordions[i].setAttribute('aria-expanded', 'false');
     }
 
+    // check if aria-expanded value is false.
     if (ariaAttribute === 'false') {
         this.setAttribute('aria-expanded', 'true');
     }
 }
 
-accordions.forEach(accordion => accordion.onclick = showAnswer);
+accordions.forEach(accordion => accordion.addEventListener('click', createAccordion));
+
+
+/*Scroll Skills*/
+
